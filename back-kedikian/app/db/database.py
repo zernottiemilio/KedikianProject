78afe60247aae58234d658c56d7337c06b8e57f5
Nacho-018 <ignacio.azumendi15@gmail.com>
2025-04-17 20:30:08 -0300

    Tablas Base de Datos

---
+++ back-kedikian/app/db/database.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:n1ngun45@localhost:5432/kedikian-db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+Base = declarative_base()
